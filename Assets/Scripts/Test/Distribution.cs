ff234bb3796321d1f455de9643065de517ccf577
Chaojie Zhu <chaojiez@andrew.ad.cmu.edu>
2017-09-24 13:01:08 -0400

    distribution test

---
+++ Assets/Scripts/Test/Distribution.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Distribution : MonoBehaviour {
+    public GameObject grass;
+    public int MaxNumber;
+    public float timeInterval;
+    public float maxTimeInterval;
+    public float minTimeInterval;
+
+    private static System.Random rnd = new System.Random();
+    private List<float> distance = new List<float>();
+    private List<Vector2> positions = new List<Vector2>();
+
+	// Use this for initialization
+	private void Start () {
+		for (int i = 0; i < MaxNumber; i++) {
+            float radius = Mathf.Sqrt((float)rnd.NextDouble()) * 5;
+            distance.Add(radius);
+            float degree = (float)rnd.NextDouble() * 360 * Mathf.Deg2Rad;
+            positions.Add(new Vector2(Mathf.Sin(degree), Mathf.Cos(degree)));
+        }
+        distance.Sort();
+        StartCoroutine(GrowGrass());
+        //for (int i = 0; i < MaxNumber; i++) {
+        //    var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        //    cube.transform.position = new Vector3(positions[i].x * distance[i], 0, positions[i].y * distance[i]);
+        //}
+    }
+
+    private IEnumerator GrowGrass() {
+        int grassNumber = 0;
+        int step = 1;
+        Vector3 position = new Vector3();
+        Quaternion rotation = Quaternion.identity;
+        while (true) {
+            for (int i = grassNumber; i < step + grassNumber && i < MaxNumber; i++) {
+                position.x = positions[i].x * distance[i];
+                position.z = positions[i].y * distance[i];
+                rotation = Quaternion.Euler(0, (float)rnd.NextDouble() * 360, 0);
+                //var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                //cube.transform.position = new Vector3(positions[i].x * distance[i], 0, positions[i].y * distance[i]);
+                var grassObject = Instantiate(grass, position, rotation);
+                grassObject.transform.localScale = (float)(rnd.NextDouble() * 0.8 + 0.2) * Vector3.one;
+            }
+            grassNumber += step;
+            step++;
+            yield return new WaitForSeconds(Mathf.Lerp(maxTimeInterval, minTimeInterval, (float)grassNumber / MaxNumber));
+            //yield return null;
+            if (grassNumber >= MaxNumber) {
+                break;
+            }
+        }
+    }
+}
--- Assets/Shader/Grass.shader
+++ Assets/Shader/Grass.shader
@@ -2,6 +2,7 @@
 	Properties{
 		_MainTex("Grass Texture", 2D) = "white" {}
 		_TimeScale("Time Scale", float) = 0.2
+		_Offset("Wave Offset", Vector) = (0, 0, 0, 0)
 		_Direction("Wind Direction", Vector) = (0, 0, 0, 0)
 	}
 
@@ -17,35 +18,48 @@
 		CGPROGRAM
 #pragma vertex vert  
 #pragma fragment frag  
-#include "UnityCG.cginc"   
+#include "UnityCG.cginc" 
+#include "Lighting.cginc"
 
 		sampler2D _MainTex;
 		half _TimeScale;
 		float4 _Direction;
+		float4 _Offset;
 
 		struct a2v {
 			float4 vertex : POSITION;
+			float3 normal : NORMAL;
 			float4 texcoord : TEXCOORD0;
 		};
 
 		struct v2f {
 			float4 pos : SV_POSITION;
 			float2 uv : TEXCOORD0;
+			fixed3 normal : TExCOORD1;
+			fixed3 worldPos : TEXCOORD2;
 		};
 
 
 		v2f vert(a2v v) {
 			v2f o;
 			float4 offset = float4(0,0,0,0);
-			float amount = sin(3.1415 * _Time.y * clamp(v.vertex.y, 0, 1))  * _TimeScale;
-			offset = _Direction * amount;
+			float amount = sin(_TimeScale * 3.1415 * _Time.y * clamp(v.vertex.y, 0, 1));
+			offset = _Direction * amount + _Offset;
 			o.pos = UnityObjectToClipPos(v.vertex + offset);
 			o.uv = v.texcoord.xy;
+			o.normal = UnityObjectToWorldNormal(v.normal);
+			o.worldPos = mul(unity_ObjectToWorld, v.vertex + offset).xyz;
 			return o;
 		}
 
-		fixed4 frag(v2f i) : SV_Target{
-			return tex2D(_MainTex, i.uv);
+		fixed4 frag(v2f i) : SV_Target {
+			fixed3 ambient = UNITY_LIGHTMODEL_AMBIENT.xyz;
+			fixed3 worldLight = normalize(UnityWorldSpaceLightDir(i.worldPos));
+			fixed3 albedo = tex2D(_MainTex, i.uv).rgb;
+			fixed3 normal = normalize(i.normal);
+			fixed3 diffuse = _LightColor0.rgb * albedo.rgb * saturate(dot(normal, worldLight));
+			fixed3 color = ambient + diffuse;
+			return fixed4(color, 1.0);
 		}
 
 			ENDCG
