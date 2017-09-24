3893e3e923b28ac3820ee5ca7589ac4dad6cd5ed
Xuefan Zhou <xuefanz@andrew.ad.cmu.edu>
2017-09-24 18:14:11 -0400

    change the pedal route for dog

--- Assets/Scripts/GameFlowManager.cs
+++ Assets/Scripts/GameFlowManager.cs
@@ -12,6 +12,10 @@ namespace BASE {
 
     public class GameFlowManager : MonoBehaviour {
         public GameState currState;
+        public Vector3 positionOfFlower;
+        public float heightOfFlower;
+        public Vector3 positionOfDog;
+
 
         public static GameFlowManager Instance;
         private Dictionary<GameState, Action> transitions = new Dictionary<GameState, Action>();
@@ -41,13 +45,13 @@ namespace BASE {
 
         private void Start() {
             Instance.Register(GameState.CALIBRATE, () => {
-                Instantiate(GameModel.Instance.flowerPrefab, new Vector3(0, GameModel.Instance.heightOffset, 2), Quaternion.identity);
+                Instantiate(GameModel.Instance.flowerPrefab, new Vector3(positionOfFlower.x, GameModel.Instance.heightOffset, positionOfFlower.z), Quaternion.identity);
             });
             Instance.Register(GameState.GRASSANIM, () => {
-                GameObject curve = Instantiate(GameModel.Instance.petalCurvePrefab, new Vector3(0, GameModel.Instance.heightOffset, 2), Quaternion.identity);
-                GameObject petal = Instantiate(GameModel.Instance.petalPrefab, new Vector3(0, GameModel.Instance.heightOffset, 2), Quaternion.identity);
+                GameObject curve = Instantiate(GameModel.Instance.petalCurvePrefab, new Vector3(positionOfFlower.x, GameModel.Instance.heightOffset + heightOfFlower, positionOfFlower.z), Quaternion.identity);
+                GameObject petal = Instantiate(GameModel.Instance.petalPrefab, new Vector3(positionOfFlower.x, GameModel.Instance.heightOffset + heightOfFlower, positionOfFlower.z), Quaternion.identity);
                 petal.GetComponent<SplineWalker>().spline = curve.GetComponent<BezierSpline>();
-                Instantiate(GameModel.Instance.dogPrefab, new Vector3(0, GameModel.Instance.heightOffset, -2), Quaternion.identity);
+                Instantiate(GameModel.Instance.dogPrefab, new Vector3(positionOfDog.x, GameModel.Instance.heightOffset, positionOfDog.z), Quaternion.identity);
             });
         }
     }
